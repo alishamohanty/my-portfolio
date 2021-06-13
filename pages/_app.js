5c99fb34ab2b51171db220e41e68863132fa883a
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-06-13 19:34:37 +0530

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-import '../styles/globals.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import ReactGA from 'react-ga'
 
 function MyApp({ Component, pageProps }) {
+
+	if (process.browser) {
+		ReactGA.initialize('G-K90QLYFF0M')
+		ReactGA.pageview(window.location.pathname + window.location.search)
+	}
+
 	return (
 		<ChakraProvider>
 			<Navbar />
